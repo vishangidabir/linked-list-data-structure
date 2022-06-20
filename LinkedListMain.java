00c1b99655bda639b8eae3fcab04291349c303a8
vishangi <vishangid@gmail.com>
2022-06-20 23:05:56 +0530

    Added : Adding integer type element to linked list

--- LinkedListMain.java
+++ LinkedListMain.java
@@ -4,18 +4,11 @@ public class LinkedListMain {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Linked List program");
-		Node firstNode = new Node(56);
-		Node secondNode = new Node(30);
-		Node thirdNode = new Node(70);
-		Node head = firstNode;
-		firstNode.next = secondNode;
-		secondNode.next = thirdNode;
-		Node tail = thirdNode;
-		Node temp = head;
-		while (temp != null) {
-			System.out.println(temp.data);
-			temp = temp.next;
-		}
-
+		LinkedList list = new LinkedList();
+        System.out.println("Adding elements");
+        list.addFirst(70);
+        list.addFirst(30);
+        list.addFirst(56);
+        list.display();
 	}
 }
