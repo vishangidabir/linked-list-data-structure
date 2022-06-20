c39da138905987c00955b69d70bb5a519130192a
vishangi <vishangid@gmail.com>
2022-06-20 23:58:58 +0530

    Added : Adding appended element to the linked list

--- LinkedList.java
+++ LinkedList.java
@@ -13,6 +13,21 @@ public class LinkedList {
 		}
 	}
 
+	public void addLast(int data) {
+		Node newNode = new Node(data);
+		if (head == null) {
+			head = newNode;
+		} else if (head.next == null){
+			head.next = newNode;
+		}else {
+			Node temp = head;
+
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+		}
+	}
 	public void display() {
 		if (head == null)
 			System.out.println("No elements to display.");
