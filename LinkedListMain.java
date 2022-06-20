51159b0a64ed6ca3bff570c5223167da9894b337
vishangi <vishangid@gmail.com>
2022-06-20 21:31:22 +0530

    Added : Create linked list

---
+++ LinkedListMain.java
@@ -0,0 +1,22 @@
+package com.bridgelabz;
+
+public class LinkedListMain {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Linked List program");
+		Node firstNode = new Node(56);
+		Node secondNode = new Node(30);
+		Node thirdNode = new Node(70);
+		Node head = firstNode;
+		firstNode.next = secondNode;
+		secondNode.next = thirdNode;
+		Node tail = thirdNode;
+
+		Node temp = head;
+		while (temp != null) {
+			System.out.println(temp.data);
+			temp = temp.next;
+		}
+
+	}
+}
