a8924c0fd6ca902116b9bd5d897fbca03273cdc5
vishangi <vishangid@gmail.com>
2022-06-21 13:42:39 +0530

    Added : Add element in between and print the list

--- Node.java
+++ Node.java
@@ -1,20 +1,30 @@
 package com.bridgelabz;
 
 public class Node {
-	  public int data;
-	    public Node next;
+	int data;
+	Node next;
 
-	    public Node(int data) {
-	        this.data = data;
-	        this.next = null;
-	    }
-
-	    @Override
-	    public String toString() {
-	        return "Node{" +
-	                "data=" + data +
-	                ", next=" + next +
-	                '}';
-	    }
-	    
+	public Node(int data) {
+		this.data = data;
 	}
+
+	public int getData() {
+		return data;
+	}
+
+	public void setData(int data) {
+		this.data = data;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
+
+	public void toPrint() {
+		System.out.println(" " + data);
+	}
+}
