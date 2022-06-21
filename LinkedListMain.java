50643f5d6148f01592f5ab90f30623464cc9ddda
vishangi <vishangid@gmail.com>
2022-06-21 14:02:51 +0530

    Added : Delete last element and print the list

--- LinkedListMain.java
+++ LinkedListMain.java
@@ -6,16 +6,16 @@ public class LinkedListMain {
 		System.out.println("Welcome to Linked List program");
 
 		LinkedList linkedList = new LinkedList();
-		System.out.println("Linked list is :");
-		linkedList.addFirstNode(56);
-		linkedList.addLast(70);
-		linkedList.toPrint();
-		System.out.println("List after adding element in middle of the list is");
-		linkedList.addInBetween(56, 70, 30);
-		linkedList.toPrint();
-		System.out.println("Linked list after deleting first element");
-		linkedList.deleteFirst();
-		linkedList.toPrint();
+        System.out.println("Linked list is :");
+        linkedList.addFirstNode(56);
+        linkedList.addLast(70);
+        linkedList.toPrint();
+        System.out.println("List after adding element in middle of the list is");
+        linkedList.addInBetween(56, 70, 30);
+        linkedList.toPrint();
+        System.out.println("Linked list after deleting last element");
+        linkedList.deleteLast();
+        linkedList.toPrint();
 
 	}
 }
