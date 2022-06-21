0ab10debb72acfaa2937d26c2a174c2a28a735d6
vishangi <vishangid@gmail.com>
2022-06-21 13:41:50 +0530

    Added : Add element in between and print the list

--- LinkedList.java
+++ LinkedList.java
@@ -1,51 +1,44 @@
 package com.bridgelabz;
 
 public class LinkedList {
-	Node head;
+	 Node head;
 
-	public void addFirst(int data) {
-		Node newNode = new Node(data);
-		if (head == null) {
-			head = newNode;
-		} else {
-			newNode.next = head;
-			head = newNode;
-		}
+	    //Method to insert elements at first node in linked list
+	    public void addFirstNode(int data) {
+	        Node node = new Node(data);
+	        node.setData(data);
+	        node.setNext(head);
+	        head = node;
+	    }
+
+	    //Method to insert elements at last node in linked list
+	    public void addLast(int data) {
+	        Node currentNode = head;
+	        while (currentNode.next != null) {
+	            currentNode = currentNode.next;
+	        }
+	        Node node = new Node(data);
+	        node.setData(data);
+	        currentNode.setNext(node);
+	    }
+
+	    //Method to print the list
+	    public void toPrint() {
+	        Node currentNode = head;
+	        while (currentNode != null) {
+	            currentNode.toPrint();
+	            currentNode = currentNode.next;
+	        }
+	    }
+
+	    //Method to insert the element in between the list
+	    public void addInBetween(int before, int after, int data) {
+	        Node currentNode = head;
+	        while (currentNode.data != before && currentNode.data != after) {
+	            currentNode = currentNode.next;
+	        }
+	        Node node = new Node(data);
+	        node.next = currentNode.next;
+	        currentNode.next = node;
+	    }
 	}
-
-	public void addLast(int data) {
-		Node newNode = new Node(data);
-		if (head == null) {
-			head = newNode;
-		} else if (head.next == null){
-			head.next = newNode;
-		}else {
-			Node temp = head;
-
-            while (temp.next != null) {
-                temp = temp.next;
-            }
-            temp.next = newNode;
-		}
-	}
-	public void display() {
-		if (head == null)
-			System.out.println("No elements to display.");
-
-		else {
-			Node temp = head;
-			while (temp != null) {
-
-				if (temp.next != null)
-					System.out.print(temp.data + " -> ");
-
-				else
-
-					System.out.println(temp.data);
-				temp = temp.next;
-
-			}
-		}
-
-	}
-}
