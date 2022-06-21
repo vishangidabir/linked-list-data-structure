ab4950d47dcd2e0f6e8e8f88ef9933068eea0f09
vishangi <vishangid@gmail.com>
2022-06-21 13:37:18 +0530

    Added : Add element in between and print the list

--- LinkedListMain.java
+++ LinkedListMain.java
@@ -2,13 +2,16 @@ package com.bridgelabz;
 
 public class LinkedListMain {
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Linked List program");
-		LinkedList list = new LinkedList();
-        System.out.println("Adding elements");
-        list.addFirst(56);
-        list.addLast(30);
-        list.addLast(70);
-        list.display();
+		public static void main(String[] args) {
+				System.out.println("Welcome to Linked List program");
+				
+				LinkedList linkedList = new LinkedList();
+		        System.out.println("Linked list is :");
+		        linkedList.addFirstNode(56);
+		        linkedList.addLast(70);
+		        linkedList.toPrint();
+		        System.out.println("List after adding element in middle of the list is");
+		        linkedList.addInBetween(56, 70, 30);
+		        linkedList.toPrint();
+		}
 	}
-}
